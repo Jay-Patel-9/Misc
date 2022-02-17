700cc740145e0c3bcf44c6e3a21086b8c4c951f1
Jay Patel <itwithjay@gmail.com>
2022-02-17 23:13:03 +0530

    Added report_type variable

--- report-parsing-in-json.sh
+++ report-parsing-in-json.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 file="test-report"
-
+report_type="aureport"
 failedlogin=$(grep -i "number of failed logins" $file  | awk -F ':' '{print $2}')
 timerange=$(grep -i "Selected time for report" $file | awk -F ':' '{print $2,$3,$4}')
 numberofusers=$(grep -i "Number of users:" $file | awk -F ':' '{print $2}' | tr -d ' ')
 
 JSON_STRING=$( jq -n \
-                  --arg tp "aureport" \
-                  --arg fl "$failedlogin" \
-                  --arg tr "$timerange" \
-                  --arg nu "$numberofusers" \
-                  '{type: $tp ,"Number of Failed Logins": $fl, "Selected time for report": $tr, "Number of users": $nu }' )
+                  --arg type "$report_type" \
+                  --arg failedlogin "$failedlogin" \
+                  --arg timerange "$timerange" \
+                  --arg numberofusers "$numberofusers" \
+                  '{Type: $type ,"Number of Failed Logins": $failedlogin, "Selected time for report": $timerange, "Number of users": $numberofusers }' )
                   
 echo "${JSON_STRING}"
